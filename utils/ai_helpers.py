826bab388729678e336a956c77dab861663b02cd
2n9qqcptj4-debug <brendan.topping@scientificgames.com>
2025-11-26 15:25:35 -0500

    Update ai_helpers.py

--- utils/ai_helpers.py
+++ utils/ai_helpers.py
@@ -1,8 +1,10 @@
 import streamlit as st
 from openai import OpenAI
 
+
 def get_client():
     return OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
 
 def call_ai(prompt: str):
     client = get_client()
