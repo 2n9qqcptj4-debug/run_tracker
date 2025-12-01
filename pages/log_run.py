ea11afacbeeceb51d0e6d33ce5a63e82c765d627
2n9qqcptj4-debug <brendan.topping@scientificgames.com>
2025-12-01 09:56:11 -0500

    Update log_run.py

--- pages/log_run.py
+++ pages/log_run.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from utils.styling import inject_css
+inject_css()
 from utils.database import add_run
 from datetime import datetime
 import pandas as pd
@@ -134,3 +136,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
